68976e1a622a5fe19a0bdd861c5728c2f517797c
Paradispartiet <paradispartiet@gmail.com>
2026-06-10 22:20:50 +0200

    Load roster readiness UI bridge

--- src/football-relationship-metric-ui.js
+++ src/football-relationship-metric-ui.js
@@ -5,6 +5,7 @@
 // inn i calculateTeamFit. Denne filen gjør scoren synlig i eksisterende UI uten
 // å røre den store app.js-filen.
 
+import "./football-roster-readiness-ui.js";
 import { calculateRoleRelationships } from "./football-relationship-engine.js";
 
 const DATA_PATHS = {
