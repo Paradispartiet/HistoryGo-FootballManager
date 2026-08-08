24cc8b4e64f1b6b1ccb73e62acffa2679538512e
Paradispartiet <paradispartiet@gmail.com>
2026-08-08 09:44:14 +0200

    Last felles valgdrawer i managerskallet

--- src/ui/manager-shell-view.js
+++ src/ui/manager-shell-view.js
@@ -1,4 +1,5 @@
 import "./manager-player-workspace-v1.js";
+import "./manager-team-choice-drawer-v1.js";
 import "./manager-scouting-workspace-v1.js";
 import "./manager-calendar-workspace-v1.js";
 import "./manager-staff-workspace-v1.js";
